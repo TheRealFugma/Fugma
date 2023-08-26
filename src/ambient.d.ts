01e498550654e79cc909dddebb7b9a6eedabf879
Edward Ji <jiziao6@gmail.com>
2023-08-26 16:01:57 +1000

    Add ambient types

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,3 +1,20 @@
+type Event = {
+    name: string
+    description: string
+    skills: string[]
+    projects: Project[]
+    participants: Person[]
+}
+
+type Project = {
+    name: string
+    skills: string[]
+    memebers: Person[]
+}
+
 type Person = {
-    name: string;
+    name: string
+    email: string
+    skills: string[]
+    interests: string[]
 }

97059f98bf3a993fbbc22af9df4e2f72ddd1f645
Edward Ji <jiziao6@gmail.com>
2023-08-26 17:21:19 +1000

    Add match algorithm scaffold and dependencies

--- src/lib/algorithm/match.ts
+++ src/lib/algorithm/match.ts
@@ -1,3 +1,20 @@
-export function match(a: Person, b) {
-    return a.toLowerCase().includes(b.toLowerCase());
+import { SequenceMatcher } from "difflib";
+
+export function stringArrSimilarity(list1: string[], list2: string[]) {
+    let score = 0;
+    for(let i = 0; i < list1.length; i++) {
+        for(let j = 0; j < list2.length; i++) {
+            score += new SequenceMatcher(null, list1[i], list2[j]).ratio();
+        }
+    }
+
+    return score;
+}
+
+export function personMatchProjects(person: Person, projects: Project[]) {
+
+}
+
+export function personMatchPeople(person: Person, people: Person[]) {
+
 }

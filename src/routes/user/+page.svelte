e7a8b89316a83921c865f44f47fd433855fe2cb4
yail259 <yaoke259@outlook.com>
2023-08-26 17:33:52 +1000

    removed deadly virus

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,15 +1,5 @@
 <script lang="ts">
-    import { createAccount } from '$lib/db/person'
-    import Button from '$lib/components/Button.svelte'
-
-    let name = 'Joe';
-    let password = '123456';
-
-
+    import { stringArrSimilarity } from "$lib/algorithm/match";
 </script>
 
-
-<Button />
-
-<input type="text" bind:value={name} />
-<input type="text" bind:value={password} />
+{stringArrSimilarity(["a", "b", "c"], ["a", "b", "c"])}
